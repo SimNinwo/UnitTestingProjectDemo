b661aaf365e9d3ee15a1619f05d73cf50d2bdd4a
Simeon Olorunninwo <Simeon.olorunninwo@wemabank.com>
2020-05-04 10:58:26 +0100

    Add project files.

---
+++ MyClassesTest/FileProcessTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyClasses;
+
+namespace MyClassesTest
+{
+    [TestClass]
+    public class FileProcessTest
+    {
+        [TestMethod]
+        public void FileNameDoesExist()
+        {
+            FileProcess fp = new FileProcess();
+            bool fromCall;
+
+            fromCall = fp.FileExists(@"C:\Windows\Regedit.exe");
+
+            Assert.IsTrue(fromCall);
+        }
+
+        [TestMethod]
+        public void FileNameDoesNotExist()
+        {
+            FileProcess fp = new FileProcess();
+            bool fromCall;
+
+            fromCall = fp.FileExists(@"C:\FileBadName.txt");
+
+            Assert.IsFalse(fromCall);
+        }
+
+        [TestMethod]
+        public void FileNameNullOrEmpty_ThrowsArgumentNullException()
+        {
+            Assert.Inconclusive();
+        }
+        //[TestMethod]
+        //public void FileNameSve()
+        //{
+        //    Assert.Inconclusive();
+        //}
+    }
+}
